a76e214958798d384fb6e375318845f3e136cb85
Mariah <mariahgardner00@gmail.com>
2025-05-05 22:41:39 -0500

    strip back react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+// Import the core React library 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Import ReactDOM for rendering into the DOM
+import ReactDOM from 'react-dom/client';
+
+// Import global CSS styles 
+import './index.css';
+
+// Import the main App component (the root of the app)
+import App from './App';
+
+// Create a root element using React 18's new createRoot API
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render App component into the root div from the <div id="root"> in public/index.html.
 root.render(
+  // React.StrictMode helps catch bugs and warnings in development.
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
